d2e874ed46f2cbdc0a61445bc1569e834c56c340
Nv7-Github <junk4Nv7@gmail.com>
2026-06-30 22:14:29 -0700

    feat: loop-timing debug pin (guarded by ENABLE_LOOP_TIMING)

---
+++ src/debug.cpp
@@ -0,0 +1,2 @@
+#include "debug.h"
+// All functionality is header-inline; this TU keeps the module linkable.
---
+++ src/debug.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "constants.h"
+#include "hardware/gpio.h"
+namespace rotev {
+inline void debugTimingInit() {
+#ifdef ENABLE_LOOP_TIMING
+  gpio_init(PIN_LOOP_TIMING); gpio_set_dir(PIN_LOOP_TIMING, GPIO_OUT); gpio_put(PIN_LOOP_TIMING, 0);
+#endif
+}
+inline void debugTimingHigh() {
+#ifdef ENABLE_LOOP_TIMING
+  sio_hw->gpio_set = 1u << PIN_LOOP_TIMING;
+#endif
+}
+inline void debugTimingLow() {
+#ifdef ENABLE_LOOP_TIMING
+  sio_hw->gpio_clr = 1u << PIN_LOOP_TIMING;
+#endif
+}
+}
